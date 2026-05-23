104b323c09bd7b7ab279dbf71867a1b802505be8
Devani-Karthikeyan <devaniselva31@gmail.com>
2026-05-23 19:39:10 +0530

    Updated add job feature and implemented get jobs functionality

--- Backend/src/main/java/com/backend/job_tracker/service/impl/JobServiceImpl.java
+++ Backend/src/main/java/com/backend/job_tracker/service/impl/JobServiceImpl.java
@@ -22,11 +22,13 @@ public class JobServiceImpl implements JobService {
     private UserRepository userRepository;
 
     @Override
-    public JobResponseDTO addJob(Long userId, JobRequestDTO jobRequestDTO) {
-        User user = userRepository.findById(userId)
+    public JobResponseDTO addJob(String email, JobRequestDTO jobRequestDTO) {
+
+        User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
         Job job = new Job();
+
         job.setTitle(jobRequestDTO.getTitle());
         job.setCompany(jobRequestDTO.getCompany());
         job.setStatus(jobRequestDTO.getStatus());
@@ -34,10 +36,10 @@ public class JobServiceImpl implements JobService {
         job.setAppliedDate(jobRequestDTO.getAppliedDate());
         job.setInterviewDate(jobRequestDTO.getInterviewDate());
         job.setNotes(jobRequestDTO.getNotes());
+
         job.setUser(user);
 
         Job savedJob = jobRepository.save(job);
-
 
         return mapToResponseDTO(savedJob);
     }
@@ -59,17 +61,27 @@ public class JobServiceImpl implements JobService {
     }
 
     @Override
-    public List<JobResponseDTO> getJobsByUserId(Long userId) {
-        User user = userRepository.findById(userId)
+    public List<JobResponseDTO> getJobsByUser(String email) {
+
+        User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
-        List<Job> jobs = jobRepository.findByUserId(userId);
+        List<Job> jobs = jobRepository.findByUserId(user.getId());
 
         return jobs.stream()
                 .map(this::mapToResponseDTO)
                 .collect(Collectors.toList());
     }
 
+    @Override
+    public JobResponseDTO getJobById(Long jobId) {
+
+        Job job = jobRepository.findById(jobId)
+                .orElseThrow(() ->
+                        new RuntimeException("Job not found"));
+
+        return mapToResponseDTO(job);
+    }
     @Override
     public JobResponseDTO updateJob(Long jobId, JobRequestDTO jobRequestDTO) {
         Job job = jobRepository.findById(jobId)
